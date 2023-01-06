1b2d966001fdf621d9b82915be1df8c512252d06
jw1036 <jw1036@gmail.com>
2023-01-06 22:47:10 +0900

    6.12 ApiService

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
 
+import 'package:flutter_toonflix/models/webtoon_detail_model.dart';
+import 'package:flutter_toonflix/models/webtoon_epsode_model.dart';
 import 'package:flutter_toonflix/models/webtoon_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -16,6 +18,30 @@ class ApiService {
       final toons = jsonDecode(response.body);
       for (var toon in toons) {
         result.add(WebtoonModel.fromJson(toon));
+      }
+      return result;
+    }
+    throw Error();
+  }
+
+  static Future<WebtoonDetailModel> getToonDetail(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final detail = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(detail);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getToonEpisodes(String id) async {
+    List<WebtoonEpisodeModel> result = [];
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+      for (var episode in episodes) {
+        result.add(WebtoonEpisodeModel.fromJson(episode));
       }
       return result;
     }
