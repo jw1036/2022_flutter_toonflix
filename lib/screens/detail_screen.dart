427847f7d44cbff2a4b0d3b38226275c7cc13674
jw1036 <jw1036@gmail.com>
2023-01-06 23:02:25 +0900

    6.14 Detail Info

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -73,6 +73,40 @@ class _DetailScreenState extends State<DetailScreen> {
                 ),
               ],
             ),
+            const SizedBox(
+              height: 25,
+            ),
+            FutureBuilder(
+              future: webtoonDetail,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 50,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          snapshot.data!.about,
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Text("${snapshot.data!.genre} / ${snapshot.data!.age}",
+                            style: const TextStyle(
+                              fontSize: 16,
+                            )),
+                      ],
+                    ),
+                  );
+                }
+                return const Text("...");
+              },
+            )
           ],
         ));
   }
